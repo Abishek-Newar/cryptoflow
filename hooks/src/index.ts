855b3bff96266981e1a1e0e37c1afb68f0f7538b
abishejk1872 <abisheknewar15@gmail.com>
2024-08-11 06:19:06 +0530

    schema updated

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
-app.post("/hooks/catch/:userId/:zapId", (req,res)=>{
+const client = new PrismaClient();
+app.post("/hooks/catch/:userId/:zapId", async(req,res)=>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
 
     //store in a db a new trigger
-
+    await client.$transaction(async tx =>{
+        await client.zapRun.create({
+            data: {
+                zapId: zapId,
+            }
+        });
+        await client.ZapRun
+    })
+    
     //push it on to a queue (kafka/redis)
+    kafkaPublisher.publish
 })
